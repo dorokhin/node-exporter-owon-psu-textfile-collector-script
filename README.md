ae891b110fc494436bce2831fb179089bc22bd9b
Andrew Dorokhin <andrew.dorokhin@icloud.com>
2024-04-12 12:39:30 +0300

    add project files

---
+++ README.md
@@ -0,0 +1,24 @@
+# Owon Programmable DC Power Supply textfile collector script for Prometheus node_exporter
+
+## Usage
+These script are to be used with the Node Exporter textfile collector.
+
+Export `OWON_PSU_PORT` environment variable
+```bash
+OWON_PSU_PORT=/dev/tty.usbserial-1440
+```
+
+## Example output
+```
+# HELP psu_measured_voltage Measured Voltage (Volts)
+# TYPE psu_measured_voltage gauge
+psu_measured_voltage{device="22472129"} 12.033
+# HELP psu_measured_current Measured Current (Amper)
+# TYPE psu_measured_current gauge
+psu_measured_current{device="22472129"} 1.129
+# HELP psu_device_info Device information
+# TYPE psu_device_info gauge
+psu_device_info{firmware_version="V4.1.0",manufacturer="OWON",model="SPE3051",serial_num="22272112"} 1.0
+```
+
+For more information see: https://github.com/prometheus/node_exporter#textfile-collector
---
+++ owon_psu_exporter.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+#
+# Prometheus node_exporter textfile collector for Owon Programmable DC Power Supply
+# (c) 2024, Andrew Dorokhin https://github.com/dorokhin
+#
+# You can find the latest version at:
+# https://github.com/dorokhin/node-exporter-owon-psu-textfile-collector-script
+#
+
+from owon_psu import OwonPSU
+from prometheus_client import CollectorRegistry, Counter, Gauge, Info, generate_latest  # noqa: E402
+import os
+
+registry = CollectorRegistry()
+namespace = 'psu'
+
+
+metrics = {
+    'measured_voltage': Gauge(
+        'measured_voltage',
+        'Measured Voltage (Volts)',
+        ['device'], namespace=namespace, registry=registry,
+    ),
+    'measured_current': Gauge(
+        'measured_current',
+        'Measured Current (Amper)',
+        ['device'], namespace=namespace, registry=registry,
+    ),
+    'device_info': Info(
+        'device',
+        'Device information',
+        ['manufacturer', 'model', 'serial_num', 'firmware_version'], namespace=namespace, registry=registry,
+    ),
+}
+
+
+def main():
+    device_port = os.environ.get('OWON_PSU_PORT', '/dev/tty.usbserial-1440')
+    with OwonPSU(device_port) as opsu:
+        manufacturer, model, serial_num, firmware_version = opsu.read_identity().split(',')
+        firmware_version = firmware_version.split(':')[1]
+        metrics['device_info'].labels(
+            manufacturer,
+            model,
+            serial_num,
+            firmware_version,
+        )
+
+        metrics['measured_voltage'].labels(serial_num).set(opsu.measure_voltage())
+        metrics['measured_current'].labels(serial_num).set(opsu.measure_current())
+
+
+if __name__ == '__main__':
+    main()
+    print(generate_latest(registry).decode(), end="")
